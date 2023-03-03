c284ffb56f56e7cae63ec4a3eeb016f72fb34596
Jim Ashford <james_a_ashford@yahoo.co.uk>
2023-03-03 22:11:23 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+Stack<int> stack = new Stack<int>();
+stack.push(20);
+stack.push(32);
+Console.WriteLine(stack.pop());
+Console.WriteLine(stack.pop());
---
+++ Stack.cs
@@ -0,0 +1,58 @@
+﻿public class Stack<T>
+{
+    private class StackNode
+    {
+        public T data;
+        public StackNode? next;
+
+        public StackNode(T data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    private StackNode? top;
+
+    public Stack()
+    {
+        top = null;
+    }
+    public Boolean isEmpty()
+    {
+        return top == null;
+    }
+    public void push(T data)
+    {
+        if (isEmpty())
+        {
+            top = new StackNode(data);
+        }
+        else
+        {
+            StackNode newTop = new StackNode(data);
+            newTop.next = top;
+            top = newTop;
+        }
+    }
+    public T? peek()
+    {
+        if (top == null)
+        {
+            return default(T);
+        }
+        else
+        {
+            return top.data;
+        }
+    }
+    public T? pop()
+    {
+        if (top == null)
+        {
+            return default(T);
+        }
+        StackNode? oldTop = top;
+        top = top.next;
+        return oldTop.data;
+    }
+}
